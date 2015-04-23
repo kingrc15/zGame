d1381f168637cd3f031134a3adcfb31955172b3d
Henry Walker Meredith <henrywmeredith@gmail.com>
2015-04-22 22:00:22 -0400

    no message

--- New Unity Project 1/Assets/Health.cs
+++ New Unity Project 1/Assets/Health.cs
@@ -8,14 +8,31 @@ public class Health : MonoBehaviour {
 
 	public GUITexture heartGUI;
 
+	private ArrayList hearts = new ArrayList();
+
+	public int maxHeartsPerRow;
+	private float spacingX;
+	private float spacingY;
+
 	// Use this for initialization
 	void Start () {
+		spacingX = heartGUI.pixelInset.width;
+		spacingY = heartGUI.pixelInset.height;
+
 		AddHearts (startinghealth / healthperheart);
 	}
 
 	public void AddHearts(int n) {
 		for (int i =0; i < n; i ++) {
-				Instantiate (heartGUI.gameObject);
+				Transform newheart = ((GameObject)Instantiate (heartGUI.gameObject)).transform;
+			newHeart.parent = transform;
+
+			int y = Mathf.FloorToInt(hearts.Count / maxHeartsPerRow);
+			int x = hears.Count - y * maxHeartsPerRow;
+
+			newheart.GetComponent<GUITexture>().pixelInset = new Rect(x * spacingX, y * spacingY,58,58);
+
+			hearts.Add (newheart);
 			}
 
 	}
